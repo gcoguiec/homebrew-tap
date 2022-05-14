1651348e22c0eb844390a4f553a82f47ed416028
Guillaume 'Yugo' Coguiec <guillaume@logical.work>
2022-05-14 10:24:15 -0400

    Add nextpnr-ecp5 recipe and update prjtrellis.

---
+++ nextpnr-ecp5.rb
@@ -0,0 +1,63 @@
+class NextpnrEcp5 < Formula
+  desc 'Portable FPGA place and route toolkit for Lattice ECP5/ECP5-5G FPGA family'
+  homepage 'https://github.com/YosysHQ/nextpnr'
+  version '0.3'
+  license 'ISC Licence'
+  url 'https://github.com/YosysHQ/nextpnr/archive/refs/tags/nextpnr-0.3.tar.gz'
+  sha256 '6dda678d369a73ca262896b672958eebeb2e6817f60afb411db31abeff191c4a'
+  head 'https://github.com/YosysHQ/nextpnr.git'
+
+  option 'without-python', 'Without python integration'
+  option 'without-heap', 'Without HeAP analytic placer'
+  option 'with-openmp', 'Use OpenMP to accelerate analytic placer'
+  option 'with-static', 'Create a static build'
+  option 'with-chipdb', 'Create build with pre-built chipdb binaries'
+  option 'with-profiler', 'Link against libprofiler'
+  option 'without-ipo', 'Compile nextpnr without IPO'
+
+  depends_on 'cmake' => :build
+  depends_on 'ninja' => :build
+  depends_on 'prjtrellis' if build.without? 'static'
+  depends_on 'prjtrellis' => :build if build.with? 'static'
+  depends_on 'boost' if build.without? 'static'
+  depends_on 'boost' => :build if build.with? 'static'
+  depends_on 'eigen' if build.without? 'static'
+  depends_on 'eigen' => :build if build.with? 'static'
+  depends_on 'python' unless build.without? 'python'
+
+  resource 'fpga-interchange-schema' do
+    url 'https://github.com/chipsalliance/fpga-interchange-schema/archive/9a48ae4d37b260e5d263287ce84e618b8e2d7f55.tar.gz'
+    sha256 '5a50912b6cc9a8a85ceb7e61964fd7d8afe3c8fb0fe84cac06e3d32e3a1f87d5'
+  end unless build.head?
+
+  resource 'fpga-interchange-schema' do
+    url 'https://github.com/chipsalliance/fpga-interchange-schema/archive/refs/heads/main.tar.gz'
+  end if build.head?
+
+  def install
+    (buildpath/'3rdparty/fpga-interchange-schema')
+      .install(resource('fpga-interchange-schema'))
+
+    args = [
+      "-DTRELLIS_INSTALL_PREFIX=#{Formula["prjtrellis"].opt_prefix}",
+      '-DARCH=ecp5'
+    ]
+    args << '-DBUILD_PYTHON=OFF' if build.without? 'python'
+    args << '-DBUILD_HEAP=OFF' if build.without? 'heap'
+    args << '-DUSE_OPENMP=ON' if build.with? 'openmp'
+    args << '-DSTATIC_BUILD=ON' if build.with? 'static'
+    args << '-DEXTERNAL_CHIPDB=ON' if build.with? 'chipdb'
+    args << '-DPROFILER=ON' if build.with? 'profiler'
+    args << '-DUSE_IPO=ON' if build.with? 'ipo'
+    args << "-DCURRENT_GIT_VERSION=#{version}" unless build.head?
+    args << "-DCURRENT_GIT_VERSION=#{head.version.commit}" if build.head?
+
+    system 'cmake', '-GNinja', *std_cmake_args, *args, '.'
+    system 'ninja', "-j#{Hardware::CPU.cores}"
+    system 'ninja', 'install'
+  end
+
+  test do
+    system "#{bin}/nextpnr-ecp5", '-h'
+  end
+end
--- prjtrellis.rb
+++ prjtrellis.rb
@@ -1,4 +1,4 @@
-class Libtrellis < Formula
+class Prjtrellis < Formula
   desc 'Open-source flow library for Lattice ECP5/ECP5-5G FPGA family'
   homepage 'https://github.com/YosysHQ/prjtrellis'
   version '1.2.1'
@@ -7,15 +7,17 @@ class Libtrellis < Formula
   sha256 '561f7881dc6d39c7ac47721e19aed533c230b47a684aa9a0c3dae08cdc3d8dbb'
   head 'https://github.com/YosysHQ/prjtrellis.git'
 
-  option 'without-python', 'Compile with no python support'
+  option 'without-python', 'Without python integration'
   option 'without-shared', 'Without libtrellis shared library'
-  option 'with-static', 'Statically compile all binaries'
+  option 'with-static', 'Create a static build'
 
   depends_on 'cmake' => :build
   depends_on 'ninja' => :build
-  depends_on 'boost'
-  depends_on 'python' => :recommended
-  depends_on 'boost-python3' => [:build, :recommended]
+  depends_on 'boost' if build.without? 'static'
+  depends_on 'boost' => :build if build.with? 'static'
+  depends_on 'python' if build.with? 'python'
+  depends_on 'boost-python3' if build.with? 'python' and build.without? 'static'
+  depends_on 'boost-python3' => :build if build.with? 'python' and build.with? 'static'
 
   resource 'prjtrellis-db' do
     url 'https://github.com/YosysHQ/prjtrellis-db/archive/35d900a94ff0db152679a67bf6e4fbf40ebc34aa.tar.gz'
@@ -28,9 +30,14 @@ class Libtrellis < Formula
 
   def install
     (buildpath/'database').install resource('prjtrellis-db')
-    args = ['-DBoost_NO_BOOST_CMAKE=ON']
+
+    args = []
+    args << '-DBUILD_PYTHON=OFF' if build.without? 'python'
+    args << '-DBUILD_SHARED=OFF' if build.without? 'shared'
+    args << '-DSTATIC_BUILD=ON' if build.with? 'static'
     args << "-DCURRENT_GIT_VERSION=#{version}" unless build.head?
     args << "-DCURRENT_GIT_VERSION=#{head.version.commit}" if build.head?
+
     cd 'libtrellis' do
       system 'cmake', '-GNinja', *std_cmake_args, *args, '.'
       system 'ninja', "-j#{Hardware::CPU.cores}"
